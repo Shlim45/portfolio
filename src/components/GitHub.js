203b8fd26f44efdbc44fb3b281eaa3f7a62555ee
Jonathan Hawranko <jhawranko@gmail.com>
2023-07-18 20:21:16 -0400

    Add octokit for fetching GitHub repositories

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -1,67 +1,17 @@
-'use client'
-
 import Link from 'next/link';
 import styles from '@/styles/GitHub.module.css'
-import { useState, useEffect } from 'react';
 
-const GitHub = () => {
-    const [projects, setProjects] = useState(null);
-
-    useEffect(() => {
-        const dummyProjects = [
-            {
-                languages: ['Java', 'MySQL'],
-                title: 'Olmran MUD',
-                description: "A Custom MUD Engine for Olmran, written in Java.  Heavily inspired by Darkness Falls: The Crusade, a MUD from the 1990's.",
-                url: 'https://www.olmran.net'
-            },
-            {
-                languages: ['JavaScript', 'React', 'Next.js', 'Supabase', 'Cloudinary'],
-                title: 'RC Custom Millworks Website',
-                description: "A server-side rendered business website created for a cabinet shop.  Includes custom CMS for updating content.",
-                url: 'https://www.rccustommillworks.com'
-            },
-            {
-                languages: ['C/C++'],
-                title: 'Olmran MUD Client',
-                description: 'A Custom MUD Client for Olmran, written in C/C++ for Windows.',
-                url: 'https://github.com/shlim45/Olmran_Client'
-            },
-            {
-                languages: ['JavaScript', 'React', 'Next.js', 'API'],
-                title: 'NHL Stats App',
-                description: 'A web application for viewing NHL statistics. Uses Next.js for rendering React components from the server.',
-                url: 'https://github.com/shlim45/nhl-stats-app'
-
-            },
-            {
-                languages: ['Go', 'MySQL'],
-                title: 'goMud',
-                description: 'MUD engine written in goLang.',
-                url: 'https://github.com/shlim45/goMud'
-
-            },
-            {
-                languages: ['Java', 'MySQL'],
-                title: 'Appointment Scheduler',
-                description: 'A GUI-based scheduling desktop application.',
-                url: 'https://github.com/shlim45/scheduler'
-            }
-        ];
-
-        setProjects(dummyProjects);
-    }, [])
-
+const GitHub = ({ repos }) => {
     return (
         <section className={styles.projects}>
             <h2>Projects</h2>
-            {projects?.map((project, index) => (
-                <Link href={`${project.url}`} rel="noopener noreferrer" target="_blank" key={index}>
+            {repos?.map((project, index) => (
+                <Link href={`${project.html_url}`} rel="noopener noreferrer" target="_blank" key={index}>
                     <div className={styles.card}>
                         <header>
-                            {project.languages?.join(', ')}
+                            {project.language}
                         </header>
-                        <h3><span>{project.title}</span></h3>
+                        <h3><span>{project.full_name}</span></h3>
                         <p>{project.description}</p>
                     </div>
                 </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,40 @@ import self from '@/../public/self.jpg'
 import styles from '@/styles/page.module.css'
 import GitHub from '@/components/GitHub'
 import Footer from '@/components/Footer'
+import { Octokit } from "octokit";
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_ACCESS_TOKEN,
+  });
+
+  const res = await octokit.request('GET /user/repos', {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+
+  // TODO(jon): Error checking
+  if (res.status !== 200) {
+    return { props: { repos: undefined } }
+  }
+
+  const repos = res.data
+    .filter(repo => {
+      const date = new Date(repo.updated_at);
+      return date.getFullYear() >= 2022 && repo.private == false;
+    })
+    .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
+
+  return {
+    props: {
+      repos,
+    },
+    revalidate: 60,
+  }
+}
+
+export default function Home({ repos }) {
   return (
     <>
       <main className={styles.main}>
@@ -26,7 +58,7 @@ export default function Home() {
           <Footer />
         </section>
 
-        <GitHub />
+        <GitHub repos={repos} />
 
       </main>
 
