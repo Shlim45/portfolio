58ee662bdba3e698c6af932dd9bd11cb91275edf
Jonathan Hawranko <jhawranko@gmail.com>
2024-02-24 16:27:01 -0500

    Updating meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,6 +43,10 @@ export default function Home({ repos }) {
     <>
       <Head>
         <title>Jonathan Hawranko</title>
+        <meta name="keywords" content="jonathan jon hawranko software web developer pittsburgh" />
+        <meta name="description" content="Full-stack web and software developer from Pittsburgh, PA." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
       </Head>
       <main className={styles.main}>
         <section className={styles.description}>
